1e84c10c5f16f30dd41ca935bdd66cb649108d39
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-07-31 15:39:14 -0700

    never type

--- src/nullable-unknown-never.ts
+++ src/nullable-unknown-never.ts
@@ -10,19 +10,25 @@
 
 // unknown type
 // kmh^-1 to ms^-1
-const getMyCarSpeed = (speed: unknown) => {
-  if (typeof speed === "number") {
-    const convertedSpeed = (speed * 1000) / 3600;
-    console.log(`My Speed Is ${convertedSpeed}`);
-  }
-  if (typeof speed === "string") {
-    const [value, unit] = speed.split(" ");
-    const convertedSpeed = (parseFloat(value) * 1000) / 3600;
-    console.log(`My Speed Is ${convertedSpeed}`);
-  } else {
-    console.log("There is wrong type");
-  }
-};
-getMyCarSpeed(10);
-getMyCarSpeed(true);
-getMyCarSpeed("11 kmh^-1");
+// const getMyCarSpeed = (speed: unknown) => {
+//   if (typeof speed === "number") {
+//     const convertedSpeed = (speed * 1000) / 3600;
+//     console.log(`My Speed Is ${convertedSpeed}`);
+//   }
+//   if (typeof speed === "string") {
+//     const [value, unit] = speed.split("  ");
+//     const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+//     console.log(`My Speed Is ${convertedSpeed}`);
+//   } else {
+//     console.log("There is wrong type");
+//   }
+// };
+// getMyCarSpeed(10);
+// getMyCarSpeed(true);
+// getMyCarSpeed("11 kmh^-1");
+
+// never type
+function throwError(message: string): never {
+  throw new Error(message);
+}
+throwError("Error hoyeche");
