3ac44f618582030af28e7c832a51c7f54b845e7b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-07-31 17:31:08 -0700

    module 1 end

--- src/question-mark.ts
+++ src/question-mark.ts
@@ -14,3 +14,24 @@ const isAuthenticatedUser = "";
 const userName = isAuthenticatedUser ?? "Guest";
 const userName2 = isAuthenticatedUser ? isAuthenticatedUser : "Guest";
 console.log({ userName }, { userName2 });
+
+type Manush = {
+  name: string;
+  age: number;
+  address: {
+    city: "No City";
+    road: "No road";
+    home?: ""; //optional type
+  };
+};
+const manush1: Manush = {
+  name: "Manush",
+  age: 100,
+  address: {
+    city: "No City",
+    road: "No road",
+  },
+};
+
+const home = manush1?.address?.home ?? "No Home";
+console.log(home);
