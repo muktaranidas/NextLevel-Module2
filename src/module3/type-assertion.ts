4f1e0dbcfb6db157af4d8a09acdde39d69991ed0
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-03 16:05:50 -0700

    type-assertion

---
+++ src/module3/type-assertion.ts
@@ -0,0 +1,18 @@
+let emni: any;
+
+emni = "Next level web development";
+
+(emni as string).length;
+
+function kgToGram(param: string | number): string | number | undefined {
+  if (typeof param === "string") {
+    const value = parseFloat(param) * 1000;
+    return `The Converted result is: ${value} gram`;
+  }
+  if (typeof param === "number") {
+    const value = param * 1000;
+    return value;
+  }
+}
+const resultToBeNumber = kgToGram(1000) as number;
+const resultToBeString = kgToGram(1000) as string;
