43f1c116fcc2f586d4d9521d7c33b8fd2bcfc400
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-27 22:12:05 -0700

    done

--- src/module3/generic-constraints.ts
+++ src/module3/generic-constraints.ts
@@ -1,13 +1,20 @@
-// const newData = { ...myInfo, crush };
+
 
 const addMeInMyCrushMind = <T>(myInfo: T) => {
   const crush = "kate Winslet";
   const newData = { ...myInfo, crush };
   return newData;
 };
-const myInfo = {
+
+type myInfoType = {
+  name: string;
+  age: number;
+  salary: number;
+};
+
+const myInfo: myInfoType = {
   name: "Mukta",
   age: 25,
   salary: 10000000000,
 };
-const result5 = addMeInMyCrushMind(myInfo);
+const result5 = addMeInMyCrushMind<myInfoType>(myInfo);
