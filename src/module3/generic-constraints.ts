e1c6e37ad1848bc6ebd08c3b28dcac434e29be25
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-29 13:32:40 -0700

    done generic contraints

--- src/module3/generic-constraints.ts
+++ src/module3/generic-constraints.ts
@@ -1,6 +1,11 @@
 
-
-const addMeInMyCrushMind = <T>(myInfo: T) => {
+type MandatoryTypes = { name: string; age: number; salary: number }
+interface MandatoryInterface { name: string; age: number; salary: number }
+const addMeInMyCrushMind = <
+  T extends MandatoryInterface
+>(
+  myInfo: T
+) => {
   const crush = "kat Winslet";
   const newData = { ...myInfo, crush };
   return newData;
@@ -10,11 +15,16 @@ type myInfoType = {
   name: string;
   age: number;
   salary: number;
+  other1: boolean;
+  other2: null;
 };
 
 const myInfo: myInfoType = {
   name: "Mukta",
   age: 25,
   salary: 10000000000,
+  other1: false,
+  other2: null,
 };
-const result5 = addMeInMyCrushMind<myInfoType>(myInfo);
+const result5 = addMeInMyCrushMind(myInfo);
+result5.
