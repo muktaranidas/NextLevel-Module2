e5996b5a76861689b6e09d46d262e5c9a97e67b9
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-29 18:20:33 -0700

    mapped type

--- src/module3/conditional-type.ts
+++ src/module3/conditional-type.ts
@@ -1,29 +1,29 @@
-// a type is dependent on another type
+// // a type is dependent on another type
 
-type a1 = string;
-type a3 = undefined;
-type a4 = number;
-type a2 = a1 extends string ? string : null;
+// type a1 = string;
+// type a3 = undefined;
+// type a4 = number;
+// type a2 = a1 extends string ? string : null;
 
-//nested conditional type
-type d = a1 extends null
-  ? null
-  : a3 extends number
-  ? number
-  : a4 extends null
-  ? null
-  : never;
+// //nested conditional type
+// type d = a1 extends null
+//   ? null
+//   : a3 extends number
+//   ? number
+//   : a4 extends null
+//   ? null
+//   : never;
 
-type Sheikh = {
-  wife1: string;
-  wife2: string;
-};
-//check korbe ei sheikh type a wife1 ase kina? true : false
-type A = keyof Sheikh;
-type CheckProperty<T, K> = K extends keyof Sheikh ? true : false;
-type CheckWife1 = CheckProperty<Sheikh, "wife12">;
+// type Sheikh = {
+//   wife1: string;
+//   wife2: string;
+// };
+// //check korbe ei sheikh type a wife1 ase kina? true : false
+// type A = keyof Sheikh;
+// type CheckProperty<T, K> = K extends keyof Sheikh ? true : false;
+// type CheckWife1 = CheckProperty<Sheikh, "wife12">;
 
-// matha kharap kora example
-type Bandhubi = "Mukta" | "Proma" | "Sajid";
-type RemoveBandhubi<T, K> = T extends K ? never : T;
-type CurrentBandhubi = RemoveBandhubi<Bandhubi, "Sajid">;
+// // matha kharap kora example
+// type Bandhubi = "Mukta" | "Proma" | "Sajid";
+// type RemoveBandhubi<T, K> = T extends K ? never : T;
+// type CurrentBandhubi = RemoveBandhubi<Bandhubi, "Sajid">;
---
+++ src/module3/mapped-types.ts
@@ -0,0 +1,42 @@
+const arrayofNumbers = [1, 2, 3];
+const arrayofStrings = arrayofNumbers.map((number) => number.toString());
+console.log(arrayofStrings);
+
+type Volume = {
+  height: number;
+  width: number;
+  depth: number;
+};
+
+type Area<T> = {
+  //   [key in keyof Volume]: Volume[key];
+  readonly [key in keyof T]: T[key];
+};
+
+const area1: Area<{ name: string }> = { name: "Mukta" };
+area1.name = 17;
+
+type AreaString = {
+  height: string;
+  width: string;
+};
+
+type AreaReadOnly = {
+  readonly height: number;
+  readonly width: number;
+};
+
+const rectangularArea: AreaNumber = {
+  height: 10,
+  width: 12,
+};
+
+rectangularArea.width = 10;
+
+type A = AreaNumber["height"]; // look up types
+type B = keyof AreaNumber;
+
+const obj = {
+  name: "Mukta",
+};
+obj["name"];
