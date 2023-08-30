e0ee30e139e71ac18376e8905648a7686abb0297
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-30 15:28:03 -0700

    inheritence done

--- src/module4/inheritence.ts
+++ src/module4/inheritence.ts
@@ -1,37 +1,55 @@
-class Student {
+class Person {
   name: string;
   age: number;
   address: string;
-
   constructor(name: string, age: number, address: string) {
     this.name = name;
     this.age = age;
     this.address = address;
   }
-
   makeSleep(hours: number): string {
     return `This ${this.name} will sleep for ${hours}`;
   }
 }
+class Student extends Person {
+  //   name: string;
+  //   age: number;
+  //   address: string;
+  constructor(name: string, age: number, address: string) {
+    super(name, age, address);
+    //   this.name = name;
+    //   this.age = age;
+    //   this.address = address;
+  }
+  //   makeSleep(hours: number): string {
+  //     return `This ${this.name} will sleep for ${hours}`;
+  //   }
+}
 
-class Teacher {
-  name: string;
-  age: number;
-  address: string;
+const student1 = new Student("Sudipto", 23, "Bashabo");
+student1.makeSleep(8);
+
+class Teacher extends Person {
+  //   name: string;
+  //   age: number;
+  //   address: string;
   designation: string;
 
   constructor(name: string, age: number, address: string, designation: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
+    super(name, age, address);
+    // this.name = name;
+    // this.age = age;
+    // this.address = address;
     this.designation = designation;
   }
 
-  makeSleep(hours: number): string {
-    return `This ${this.name} will sleep for ${hours}`;
-  }
+  //   makeSleep(hours: number): string {
+  //     return `This ${this.name} will sleep for ${hours}`;
+  //   }
 
   takeClasses(numberOfClass: number): string {
     return `This ${this.name} will take ${numberOfClass} classes`;
   }
 }
+const teacher1 = new Teacher("Mukta", 25, "Lalmatia", "SWE");
+teacher1.name;
