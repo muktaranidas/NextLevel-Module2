1c83eb9dabfcbd78c22683c0f29656e742e64024
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-30 14:00:41 -0700

    completely done create class,Object,Parameter Properties

--- src/module4/object.ts
+++ src/module4/object.ts
@@ -1,21 +1,39 @@
 //Class
-class Animal {
-  name: string;
-  species: string;
-  sound: string;
+// class Animal {
+//   public name: string;
+//   public species: string;
+//   public sound: string;
+//Constractor
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
 
-  //Constractor
-  constructor(name: string, species: string, sound: string) {
-    this.name = name;
-    this.species = species;
-    this.sound = sound;
+//Class
+class Animal {
+  //   public name: string;
+  //   public species: string;
+  //   public sound: string;
+
+  //Parameter properties
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
   }
 
   //function
-  makeSound() {
+  public makeSound() {
     console.log(`The ${this.name} says ${this.sound} `);
   }
 }
 //intents
 const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+const cat = new Animal("Sudipto", "cat", "meaw");
 dog.makeSound();
+cat.makeSound();
