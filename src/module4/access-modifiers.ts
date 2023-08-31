19809e4ad47660d4fb3c7cc3a9d69f2bc59f52b4
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-08-31 14:34:09 -0700

    getter and setter star

--- src/module4/access-modifiers.ts
+++ src/module4/access-modifiers.ts
@@ -1,27 +1,27 @@
-class BankAccount {
-  public id: number;
-  public name: string;
-  private balance: number;
+// class BankAccount {
+//   public id: number;
+//   public name: string;
+//   private balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this.balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this.balance = balance;
+//   }
 
-  getBalance() {
-    console.log(`My Current Balance is: ${this.balance}`);
-  }
-  addDeposit(account: number) {
-    this.balance = this.balance + account;
-  }
-}
+//   getBalance() {
+//     console.log(`My Current Balance is: ${this.balance}`);
+//   }
+//   addDeposit(account: number) {
+//     this.balance = this.balance + account;
+//   }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    this.getBalance();
-  }
-}
-const myAccount = new BankAccount(2733, "Mukta", 100);
-// myAccount.balance = 0; //cannt access coz is private modifier
-console.log(myAccount);
+// class StudentAccount extends BankAccount {
+//   test() {
+//     this.getBalance();
+//   }
+// }
+// const myAccount = new BankAccount(2733, "Mukta", 100);
+// // myAccount.balance = 0; //cannt access coz is private modifier
+// console.log(myAccount);
