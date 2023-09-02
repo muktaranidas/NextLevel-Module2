b10d4f83a55a66e59dd2347eb3bc3ce18d8b91ca
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-09-01 22:54:20 -0700

    done

--- src/module4/static.ts
+++ src/module4/static.ts
@@ -1,18 +1,15 @@
 class Counter {
-  counter: number;
+  static counter: number = 0;
 
-  constructor(counter: number) {
-    this.counter = counter;
-  }
   increment(): number {
-    return (this.counter = this.counter + 1);
+    return (Counter.counter = Counter.counter + 1);
   }
   decrement(): number {
-    return (this.counter = this.counter - 1);
+    return (Counter.counter = Counter.counter - 1);
   }
 }
 
-const instance1 = new Counter(0);
-const instance2 = new Counter(1);
+const instance1 = new Counter();
+const instance2 = new Counter();
 console.log(instance1.increment());
 console.log(instance2.increment());
