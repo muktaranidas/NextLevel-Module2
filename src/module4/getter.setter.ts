f5716597ba8487d627434a77f470e005ec653ab9
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-09-01 22:48:15 -0700

    static class

--- src/module4/getter.setter.ts
+++ src/module4/getter.setter.ts
@@ -1,42 +1,42 @@
-class BankAccount {
-  public id: number;
-  public name: string;
-  private balance: number;
+// class BankAccount {
+//   public id: number;
+//   public name: string;
+//   private balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this.balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this.balance = balance;
+//   }
 
-  //getter
-  get balanceGet(): number {
-    return this.balance;
-  }
+//   //getter
+//   get balanceGet(): number {
+//     return this.balance;
+//   }
 
-  // setter
-  set deposit(amount: number) {
-    this.balance = this.balance + amount;
-  }
+//   // setter
+//   set deposit(amount: number) {
+//     this.balance = this.balance + amount;
+//   }
 
-  getBalance(): number {
-    return this.balance;
-  }
-  // addDeposit(account: number) {
-  //   this.balance = this.balance + account;
-  // }
-}
+//   getBalance(): number {
+//     return this.balance;
+//   }
+//   // addDeposit(account: number) {
+//   //   this.balance = this.balance + account;
+//   // }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    // this.getBalance();
-  }
-}
-const myAccount = new BankAccount(2733, "Mukta", 100);
-// myAccount.balance = 0; //cannt access coz is private modifier
-// myAccount.addDeposit(20);
-// myAccount.getBalance();
-// myAccount.getBalance();
-console.log(myAccount.balanceGet);
-myAccount.deposit = 29;
-console.log(myAccount.balanceGet);
+// class StudentAccount extends BankAccount {
+//   test() {
+//     // this.getBalance();
+//   }
+// }
+// const myAccount = new BankAccount(2733, "Mukta", 100);
+// // myAccount.balance = 0; //cannt access coz is private modifier
+// // myAccount.addDeposit(20);
+// // myAccount.getBalance();
+// // myAccount.getBalance();
+// console.log(myAccount.balanceGet);
+// myAccount.deposit = 29;
+// console.log(myAccount.balanceGet);
