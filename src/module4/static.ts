d04950f587368ebf96f3d4557e7e9cabf3a0604b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-09-02 21:33:22 -0700

    done

--- src/module4/static.ts
+++ src/module4/static.ts
@@ -1,11 +1,11 @@
-class Counter {
-  static counter: number = 0;
-  static increment(): number {
-    return (Counter.counter = Counter.counter + 1);
-  }
-  static decrement(): number {
-    return (Counter.counter = Counter.counter - 1);
-  }
-}
-console.log(Counter.increment());
-console.log(Counter.increment());
+// class Counter {
+//   static counter: number = 0;
+//   static increment(): number {
+//     return (Counter.counter = Counter.counter + 1);
+//   }
+//   static decrement(): number {
+//     return (Counter.counter = Counter.counter - 1);
+//   }
+// }
+// console.log(Counter.increment());
+// console.log(Counter.increment());
